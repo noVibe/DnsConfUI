4c0a95444e2cea925681bb19ceed3d5410c4c101
Vasiliy <remindscope@gmail.com>
2026-06-08 00:03:24 +0300

    Rename OISD constants, fix GEOBLOCK_HOSTS_URL

--- src/domain/toggles.ts
+++ src/domain/toggles.ts
@@ -1,7 +1,11 @@
-export const GEOBLOCK_HOSTS_URL = [
-  "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild2_small.txt",
-  "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild_small.txt"
-];
+export const GEOBLOCK_HOSTS_URL =
+  "https://raw.githubusercontent.com/Internet-Helper/GeoHideDNS/refs/heads/main/hosts/hosts";
+
+export const OISD_SMALL_BLOCK_SUBDOMAINS =
+  "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild2_small.txt";
+
+export const OISD_SMALL_BLOCK_DOMAINS =
+  "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild_small.txt";
 
 export const ADS_BLOCKLIST_IDS = [
   "nextdns-recommended",
